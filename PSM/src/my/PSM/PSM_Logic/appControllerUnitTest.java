9b60931f5dc4585c0d85bb4787ad182aa05a06f4
Joseph Adams <jadam090@fiu.edu>
2019-02-15 12:47:27 -0500

    added two cases per method in AppCon

--- PSM/src/my/PSM/PSM_Logic/appControllerUnitTest.java
+++ PSM/src/my/PSM/PSM_Logic/appControllerUnitTest.java
@@ -7,6 +7,10 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class appControllerUnitTest {
+	/*
+	 * Reference tables
+	 * 
+	 */
 
 	@Before
 	public void setUp() throws Exception {
@@ -16,114 +20,533 @@ public class appControllerUnitTest {
 	public void tearDown() throws Exception {
 	}
 
+	/*
+	 * Test Id: AC_001
+	 * Test Purpose: 
+	 * Setup: 
+	 * Input:
+	 * Expected Output: 
+	 */
 	@Test
-	public void testAppController() {
+	public void testAppController1() {
+		fail("Not yet implemented");
+	}
+	
+	/*
+	 * Test Id: AC_001
+	 * Test Purpose: 
+	 * Setup: 
+	 * Input:
+	 * Expected Output: 
+	 */
+	@Test
+	public void testAppController2() {
 		fail("Not yet implemented");
 	}
 
+	/*
+	 * Test Id: AC_001
+	 * Test Purpose: 
+	 * Setup: 
+	 * Input:
+	 * Expected Output: 
+	 */
 	@Test
-	public void testMain() {
+	public void testMain1() {
 		fail("Not yet implemented");
 	}
 
+	/*
+	 * Test Id: AC_001
+	 * Test Purpose: 
+	 * Setup: 
+	 * Input:
+	 * Expected Output: 
+	 */
 	@Test
-	public void testCheckClear() {
+	public void testMain2() {
+		fail("Not yet implemented");
+	}
+	
+	/*
+	 * Test Id: AC_001
+	 * Test Purpose: 
+	 * Setup: 
+	 * Input:
+	 * Expected Output: 
+	 */
+	@Test
+	public void testCheckClear1() {
 		fail("Not yet implemented");
 	}
 
+	/*
+	 * Test Id: AC_001
+	 * Test Purpose: 
+	 * Setup: 
+	 * Input:
+	 * Expected Output: 
+	 */
 	@Test
-	public void testCheckTimes() {
+	public void testCheckClear2() {
+		fail("Not yet implemented");
+	}
+	
+	/*
+	 * Test Id: AC_001
+	 * Test Purpose: 
+	 * Setup: 
+	 * Input:
+	 * Expected Output: 
+	 */
+	@Test
+	public void testCheckTimes1() {
 		fail("Not yet implemented");
 	}
 
+	/*
+	 * Test Id: AC_001
+	 * Test Purpose: 
+	 * Setup: 
+	 * Input:
+	 * Expected Output: 
+	 */
 	@Test
-	public void testGetData() {
+	public void testCheckTimes2() {
+		fail("Not yet implemented");
+	}
+	
+	/*
+	 * Test Id: AC_001
+	 * Test Purpose: 
+	 * Setup: 
+	 * Input:
+	 * Expected Output: 
+	 */
+	@Test
+	public void testGetData1() {
 		fail("Not yet implemented");
 	}
 
+	/*
+	 * Test Id: AC_001
+	 * Test Purpose: 
+	 * Setup: 
+	 * Input:
+	 * Expected Output: 
+	 */
 	@Test
-	public void testSleep() {
+	public void testGetData2() {
+		fail("Not yet implemented");
+	}
+	
+	/*
+	 * Test Id: AC_001
+	 * Test Purpose: 
+	 * Setup: 
+	 * Input:
+	 * Expected Output: 
+	 */
+	@Test
+	public void testSleep1() {
 		fail("Not yet implemented");
 	}
 
+	/*
+	 * Test Id: AC_001
+	 * Test Purpose: 
+	 * Setup: 
+	 * Input:
+	 * Expected Output: 
+	 */
 	@Test
-	public void testLogIn() {
+	public void testSleep2() {
+		fail("Not yet implemented");
+	}
+	
+	/*
+	 * Test Id: AC_001
+	 * Test Purpose: 
+	 * Setup: 
+	 * Input:
+	 * Expected Output: 
+	 */
+	@Test
+	public void testLogIn1() {
+		fail("Not yet implemented");
+	}
+	
+	/*
+	 * Test Id: AC_001
+	 * Test Purpose: 
+	 * Setup: 
+	 * Input:
+	 * Expected Output: 
+	 */
+	@Test
+	public void testLogIn2() {
 		fail("Not yet implemented");
 	}
 
+	/*
+	 * Test Id: AC_001
+	 * Test Purpose: 
+	 * Setup: 
+	 * Input:
+	 * Expected Output: 
+	 */
 	@Test
-	public void testGetCon() {
+	public void testGetCon1() {
 		fail("Not yet implemented");
 	}
 
+	/*
+	 * Test Id: AC_001
+	 * Test Purpose: 
+	 * Setup: 
+	 * Input:
+	 * Expected Output: 
+	 */
 	@Test
-	public void testSetTime() {
+	public void testGetCon2() {
+		fail("Not yet implemented");
+	}
+	
+	/*
+	 * Test Id: AC_001
+	 * Test Purpose: 
+	 * Setup: 
+	 * Input:
+	 * Expected Output: 
+	 */
+	@Test
+	public void testSetTime1() {
 		fail("Not yet implemented");
 	}
 
+	/*
+	 * Test Id: AC_001
+	 * Test Purpose: 
+	 * Setup: 
+	 * Input:
+	 * Expected Output: 
+	 */
 	@Test
-	public void testGetTime() {
+	public void testSetTime2() {
+		fail("Not yet implemented");
+	}
+	
+	/*
+	 * Test Id: AC_001
+	 * Test Purpose: 
+	 * Setup: 
+	 * Input:
+	 * Expected Output: 
+	 */
+	@Test
+	public void testGetTime1() {
 		fail("Not yet implemented");
 	}
 
+	/*
+	 * Test Id: AC_001
+	 * Test Purpose: 
+	 * Setup: 
+	 * Input:
+	 * Expected Output: 
+	 */
 	@Test
-	public void testGetTimeMillis() {
+	public void testGetTime2() {
+		fail("Not yet implemented");
+	}
+	
+	/*
+	 * Test Id: AC_001
+	 * Test Purpose: 
+	 * Setup: 
+	 * Input:
+	 * Expected Output: 
+	 */
+	@Test
+	public void testGetTimeMillis1() {
+		fail("Not yet implemented");
+	}
+	
+	/*
+	 * Test Id: AC_001
+	 * Test Purpose: 
+	 * Setup: 
+	 * Input:
+	 * Expected Output: 
+	 */
+	@Test
+	public void testGetTimeMillis2() {
 		fail("Not yet implemented");
 	}
 
+	/*
+	 * Test Id: AC_001
+	 * Test Purpose: 
+	 * Setup: 
+	 * Input:
+	 * Expected Output: 
+	 */
 	@Test
-	public void testTimerParser() {
+	public void testTimerParser1() {
+		fail("Not yet implemented");
+	}
+	
+	/*
+	 * Test Id: AC_001
+	 * Test Purpose: 
+	 * Setup: 
+	 * Input:
+	 * Expected Output: 
+	 */
+	@Test
+	public void testTimerParser2() {
 		fail("Not yet implemented");
 	}
 
+	/*
+	 * Test Id: AC_001
+	 * Test Purpose: 
+	 * Setup: 
+	 * Input:
+	 * Expected Output: 
+	 */
 	@Test
-	public void testDateParser() {
+	public void testDateParser1() {
 		fail("Not yet implemented");
 	}
 
+	/*
+	 * Test Id: AC_001
+	 * Test Purpose: 
+	 * Setup: 
+	 * Input:
+	 * Expected Output: 
+	 */
 	@Test
-	public void testReturnHr() {
+	public void testDateParser2() {
+		fail("Not yet implemented");
+	}
+	
+	/*
+	 * Test Id: AC_001
+	 * Test Purpose: 
+	 * Setup: 
+	 * Input:
+	 * Expected Output: 
+	 */
+	@Test
+	public void testReturnHr1() {
+		fail("Not yet implemented");
+	}
+	
+	/*
+	 * Test Id: AC_001
+	 * Test Purpose: 
+	 * Setup: 
+	 * Input:
+	 * Expected Output: 
+	 */
+	@Test
+	public void testReturnHr2() {
 		fail("Not yet implemented");
 	}
 
+	/*
+	 * Test Id: AC_001
+	 * Test Purpose: 
+	 * Setup: 
+	 * Input:
+	 * Expected Output: 
+	 */
 	@Test
-	public void testReturnMin() {
+	public void testReturnMin1() {
+		fail("Not yet implemented");
+	}
+	
+	/*
+	 * Test Id: AC_001
+	 * Test Purpose: 
+	 * Setup: 
+	 * Input:
+	 * Expected Output: 
+	 */
+	@Test
+	public void testReturnMin2() {
 		fail("Not yet implemented");
 	}
 
+	/*
+	 * Test Id: AC_001
+	 * Test Purpose: 
+	 * Setup: 
+	 * Input:
+	 * Expected Output: 
+	 */
 	@Test
-	public void testGetEndTime() {
+	public void testGetEndTime1() {
+		fail("Not yet implemented");
+	}
+	
+	/*
+	 * Test Id: AC_001
+	 * Test Purpose: 
+	 * Setup: 
+	 * Input:
+	 * Expected Output: 
+	 */
+	@Test
+	public void testGetEndTime2() {
 		fail("Not yet implemented");
 	}
 
+	/*
+	 * Test Id: AC_001
+	 * Test Purpose: 
+	 * Setup: 
+	 * Input:
+	 * Expected Output: 
+	 */
 	@Test
-	public void testSetSemesterClear() {
+	public void testSetSemesterClear1() {
+		fail("Not yet implemented");
+	}
+	
+	/*
+	 * Test Id: AC_001
+	 * Test Purpose: 
+	 * Setup: 
+	 * Input:
+	 * Expected Output: 
+	 */
+	@Test
+	public void testSetSemesterClear2() {
 		fail("Not yet implemented");
 	}
 
+	/*
+	 * Test Id: AC_001
+	 * Test Purpose: 
+	 * Setup: 
+	 * Input:
+	 * Expected Output: 
+	 */
 	@Test
-	public void testGetSemesterClear() {
+	public void testGetSemesterClear1() {
+		fail("Not yet implemented");
+	}
+	
+	/*
+	 * Test Id: AC_001
+	 * Test Purpose: 
+	 * Setup: 
+	 * Input:
+	 * Expected Output: 
+	 */
+	@Test
+	public void testGetSemesterClear2() {
 		fail("Not yet implemented");
 	}
 
+	/*
+	 * Test Id: AC_001
+	 * Test Purpose: 
+	 * Setup: 
+	 * Input:
+	 * Expected Output: 
+	 */
 	@Test
-	public void testGet15BeforeEnd() {
+	public void testGet15BeforeEnd1() {
 		fail("Not yet implemented");
 	}
 
+	/*
+	 * Test Id: AC_001
+	 * Test Purpose: 
+	 * Setup: 
+	 * Input:
+	 * Expected Output: 
+	 */
 	@Test
-	public void testGet5BeforeEnd() {
+	public void testGet15BeforeEnd2() {
+		fail("Not yet implemented");
+	}
+	
+	/*
+	 * Test Id: AC_001
+	 * Test Purpose: 
+	 * Setup: 
+	 * Input:
+	 * Expected Output: 
+	 */
+	@Test
+	public void testGet5BeforeEnd1() {
 		fail("Not yet implemented");
 	}
 
+	/*
+	 * Test Id: AC_001
+	 * Test Purpose: 
+	 * Setup: 
+	 * Input:
+	 * Expected Output: 
+	 */
 	@Test
-	public void testAutoExit() {
+	public void testGet5BeforeEnd2() {
+		fail("Not yet implemented");
+	}
+	
+	/*
+	 * Test Id: AC_001
+	 * Test Purpose: 
+	 * Setup: 
+	 * Input:
+	 * Expected Output: 
+	 */
+	@Test
+	public void testAutoExit1() {
 		fail("Not yet implemented");
 	}
 
+	/*
+	 * Test Id: AC_001
+	 * Test Purpose: 
+	 * Setup: 
+	 * Input:
+	 * Expected Output: 
+	 */
 	@Test
-	public void testAutoClear() {
+	public void testAutoExit2() {
 		fail("Not yet implemented");
 	}
+	
+	/*
+	 * Test Id: AC_001
+	 * Test Purpose: 
+	 * Setup: 
+	 * Input:
+	 * Expected Output: 
+	 */
+	@Test
+	public void testAutoClear1() {
+		fail("Not yet implemented");
+	}
+	
+	/*
+	 * Test Id: AC_001
+	 * Test Purpose: 
+	 * Setup: 
+	 * Input:
+	 * Expected Output: 
+	 */
+	@Test
+	public void testAutoClear2() {
+		fail("Not yet implemented");
+	}
+	
 
 }
