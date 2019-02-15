b5e635a480af3c0c85c12e780cf19b3d2b1f63f6
Joseph Adams <jadam090@fiu.edu>
2019-02-15 11:54:28 -0500

    added comment stubs

---
+++ PSM/src/my/PSM/PSM_Logic/appControllerUnitTest.java
@@ -0,0 +1,129 @@
+package my.PSM.PSM_Logic;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class appControllerUnitTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testAppController() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMain() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCheckClear() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCheckTimes() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetData() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSleep() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testLogIn() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetCon() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetTime() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetTime() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetTimeMillis() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testTimerParser() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDateParser() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testReturnHr() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testReturnMin() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetEndTime() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetSemesterClear() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetSemesterClear() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGet15BeforeEnd() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGet5BeforeEnd() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAutoExit() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAutoClear() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ PSM/src/my/PSM/PSM_Storage/DBConnectionUnitTest.java
@@ -0,0 +1,164 @@
+package my.PSM.PSM_Storage;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class DBConnectionUnitTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testDBConnection() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testConnectStringStringString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testConnectStringString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDisconnect() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFetchCourseID() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetEndDates() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetCourses() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFetchCourses() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFetchCourseSubj() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFetchCourseName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFetchCourseSemester() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFetchCourseStart() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFetchCourseEnd() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFetchStartMon() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFetchEndMon() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFetchStartTue() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFetchEndTue() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFetchStartWed() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFetchEndWed() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFetchStartThu() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFetchEndThu() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFetchStartFri() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFetchEndFri() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFetchStartSat() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFetchEndSat() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testStoreClassInfo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testStoreClassSched() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testClearDatabase() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCreateClassTable() {
+		fail("Not yet implemented");
+	}
+
+}
