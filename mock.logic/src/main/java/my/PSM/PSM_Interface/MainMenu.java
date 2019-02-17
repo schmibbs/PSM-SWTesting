78447884941197498efbf587942c70b0993b93be
Esteban Huezo <46468491+EstebanH@users.noreply.github.com>
2019-02-16 19:06:16 -0500

    File Upload
    Created stub for DBConnection.
    JUnit for Auth

---
+++ mock.logic/src/main/java/my/PSM/PSM_Interface/MainMenu.java
@@ -0,0 +1,44 @@
+/*
+ * MainMenu2.java
+ *
+ * Created on April 13, 2008, 2:47 PM
+ */
+
+package my.PSM.PSM_Interface;
+
+/**
+ *
+ * @author  Kurt
+ */
+public class MainMenu extends javax.swing.JFrame {
+
+	public void launchForm() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public boolean dataRec() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean editSchedSelected() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean InitSetupSelected() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean logoutSelected() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public void setdataRec(boolean b) {
+		// TODO Auto-generated method stub
+		
+	}
+}
---
+++ mock.logic/src/main/java/my/PSM/PSM_Interface/endOfClass.java
@@ -0,0 +1,14 @@
+/*
+ * endOfClass.java
+ *
+ * Created on April 15, 2008, 1:17 PM
+ */
+
+package my.PSM.PSM_Interface;
+
+/**
+ *
+ * @author  Kurt
+ */
+public class endOfClass extends javax.swing.JFrame {
+}
